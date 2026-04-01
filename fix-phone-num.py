4f4be720d157042a236b924f54466886e9cd553c
sydneyf10 <166837026+sydneyf10@users.noreply.github.com>
2026-04-01 16:21:44 -0500

    Update fix-phone-num.py

--- fix-phone-num.py
+++ fix-phone-num.py
@@ -2,17 +2,13 @@ import pytest
 
 def fix_phone_num(phone_num_to_fix):
     if len(phone_num_to_fix) not in [10, 11]:
-        raise ValueError(f"phone number must be length 10; got \"{phone_num_to_fix}\" which is of length {len(phone_num_to_fix)}")
+        raise ValueError(f"phone number must be length 10 or 11; got \"{phone_num_to_fix}\" which is of length {len(phone_num_to_fix)}")
     if not phone_num_to_fix.isdigit():
         raise ValueError(f"phone number must only contain digits; got \"{phone_num_to_fix}\"")
     if len(phone_num_to_fix) == 11:
-        # The phone number is of length 11, so check if it has the "1" country code.
-        if phone_num_to_fix[0] != "1":
-            # The country code wasn't a "1", so the number is invalid.
-            raise ValueError(f"phone numbers of length 11 must begin with a 1, got \"{phone_num_to_fix}\"")
-        # Strip off the country code "1" from the beginning.
-        # Resulting string should be of length 10 to work with formatting code below.
-        phone_num_to_fix = phone_num_to_fix[1:]
+    if phone_num_to_fix[0] != "1":
+        raise ValueError(f"phone numbers of length 11 must begin with a 1, got \"{phone_num_to_fix}\"")
+    phone_num_to_fix = phone_num_to_fix[1:]
 
     # given "5125558823". Split the parts, then recombine and return
     area_code = phone_num_to_fix[0:3]  # 512 (first three digits)
